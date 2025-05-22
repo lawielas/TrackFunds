db4bdda248c3af2fa49c7718f7a11f32d842cc32
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2025-05-22 12:23:09 +0100

    auth pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,78 @@
-import { Text, View } from "react-native";
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View } from "react-native";
+
+const authImage = require('@/assets/images/auth_vector.png')
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={authImage} style={styles.image} />
+      <Text style={styles.title}>Grow Your Savings,</Text>
+      <Text style={styles.title}>Once Step at a Time</Text>
+      <Text style={styles.subHead}>See your progress, get reminders, and start building the habits that lead to financial freedom.</Text>
+      <View style={styles.linksContainer}>
+        <Link href='/signup' style={styles.start}>
+          Get Started
+        </Link>
+        <Link href='/signin' style={styles.continue}>
+          Sign in
+        </Link>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 20,
+    justifyContent: 'center',
+    backgroundColor: '#E6ECF4'
+  },
+  image: {
+    width: 320,
+    height: 320,
+  },
+  title: {
+    fontSize: 33,
+    fontWeight: '700'
+  },
+  subHead: {
+    textAlign: 'center',
+    marginTop: 10,
+    paddingHorizontal: 30,
+    fontSize: 16
+  },
+  linksContainer: {
+    gap: 10,
+    marginTop: 20
+  },
+  start: {
+    backgroundColor: '#264169',
+    color: '#fff',
+    width: 250,
+    textAlign: 'center',
+    fontSize: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#264169'
+  },
+  continue: {
+    color: '#264169',
+    width: 250,
+    textAlign: 'center',
+    fontSize: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#264169'
+  }
+})
+
+
+
+
+
---
+++ app/signin.tsx
@@ -0,0 +1,62 @@
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { KeyboardAvoidingView, Platform, StyleSheet, TextInput } from "react-native";
+
+const signupImage = require('@/assets/images/signin.png')
+
+
+export default function signin() {
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={styles.container}>
+      <Image source={signupImage} style={styles.image} />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Link href='/signup' style={styles.signupButton}>
+        Sign in
+      </Link>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 20,
+    justifyContent: 'center',
+    backgroundColor: '#E6ECF4'
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    fontSize: 16,
+    borderColor: '#264169'
+  },
+  signupButton: {
+    backgroundColor: '#264169',
+    marginTop: 30,
+    color: '#fff',
+    width: '80%',
+    textAlign: 'center',
+    fontSize: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#264169'
+  },
+})
